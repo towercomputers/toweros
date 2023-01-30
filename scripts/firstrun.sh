0f67fe697cd4e9f9d4e38e4f51f8fa308e831e4a
Ouziel Slama <ouziel@stashblox.com>
2023-01-30 22:03:25 +0100

    scripts template

--- scripts/firstrun.sh
+++ scripts/firstrun.sh
@@ -1,5 +1,80 @@
-#!/bin/sh
+#!/bin/bash
 
-echo "Bogus firstrun.sh file"
+set +e
 
+CURRENT_HOSTNAME=`cat /etc/hostname | tr -d " \t\n\r"`
+if [ -f /usr/lib/raspberrypi-sys-mods/imager_custom ]; then
+   /usr/lib/raspberrypi-sys-mods/imager_custom set_hostname office
+else
+   echo office >/etc/hostname
+   sed -i "s/127.0.1.1.*$CURRENT_HOSTNAME/127.0.1.1\toffice/g" /etc/hosts
+fi
+FIRSTUSER=`getent passwd 1000 | cut -d: -f1`
+FIRSTUSERHOME=`getent passwd 1000 | cut -d: -f6`
+if [ -f /usr/lib/raspberrypi-sys-mods/imager_custom ]; then
+   /usr/lib/raspberrypi-sys-mods/imager_custom enable_ssh -k 'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKDpWFEwDzjCCjLfo2fpQAqZAjYsvusN3r2KVwdeXEnd office'
+else
+   install -o "$FIRSTUSER" -m 700 -d "$FIRSTUSERHOME/.ssh"
+   install -o "$FIRSTUSER" -m 600 <(printf "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKDpWFEwDzjCCjLfo2fpQAqZAjYsvusN3r2KVwdeXEnd office") "$FIRSTUSERHOME/.ssh/authorized_keys"
+   echo 'PasswordAuthentication no' >>/etc/ssh/sshd_config
+   systemctl enable ssh
+fi
+if [ -f /usr/lib/userconf-pi/userconf ]; then
+   /usr/lib/userconf-pi/userconf 'tower' '$5$.mdsQYdiaf$h0ByWZ8XDVL4x.E3DA1KRff8tGg5jghO4gCxi1sXJU3'
+else
+   echo "$FIRSTUSER:"'$5$.mdsQYdiaf$h0ByWZ8XDVL4x.E3DA1KRff8tGg5jghO4gCxi1sXJU3' | chpasswd -e
+   if [ "$FIRSTUSER" != "tower" ]; then
+      usermod -l "tower" "$FIRSTUSER"
+      usermod -m -d "/home/tower" "tower"
+      groupmod -n "tower" "$FIRSTUSER"
+      if grep -q "^autologin-user=" /etc/lightdm/lightdm.conf ; then
+         sed /etc/lightdm/lightdm.conf -i -e "s/^autologin-user=.*/autologin-user=tower/"
+      fi
+      if [ -f /etc/systemd/system/getty@tty1.service.d/autologin.conf ]; then
+         sed /etc/systemd/system/getty@tty1.service.d/autologin.conf -i -e "s/$FIRSTUSER/tower/"
+      fi
+      if [ -f /etc/sudoers.d/010_pi-nopasswd ]; then
+         sed -i "s/^$FIRSTUSER /tower /" /etc/sudoers.d/010_pi-nopasswd
+      fi
+   fi
+fi
+if [ -f /usr/lib/raspberrypi-sys-mods/imager_custom ]; then
+   /usr/lib/raspberrypi-sys-mods/imager_custom set_wlan 'Bbox-BDC08515' '1f89811290ae7bbe7ded12754183677c216d4f28a78ff110555f51b08573754a' 'FR'
+else
+cat >/etc/wpa_supplicant/wpa_supplicant.conf <<'WPAEOF'
+country=FR
+ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev
+ap_scan=1
+
+update_config=1
+network={
+	ssid="Bbox-BDC08515"
+	psk=1f89811290ae7bbe7ded12754183677c216d4f28a78ff110555f51b08573754a
+}
+
+WPAEOF
+   chmod 600 /etc/wpa_supplicant/wpa_supplicant.conf
+   rfkill unblock wifi
+   for filename in /var/lib/systemd/rfkill/*:wlan ; do
+       echo 0 > $filename
+   done
+fi
+if [ -f /usr/lib/raspberrypi-sys-mods/imager_custom ]; then
+   /usr/lib/raspberrypi-sys-mods/imager_custom set_keymap 'fr'
+   /usr/lib/raspberrypi-sys-mods/imager_custom set_timezone 'Europe/Paris'
+else
+   rm -f /etc/localtime
+   echo "Europe/Paris" >/etc/timezone
+   dpkg-reconfigure -f noninteractive tzdata
+cat >/etc/default/keyboard <<'KBEOF'
+XKBMODEL="pc105"
+XKBLAYOUT="fr"
+XKBVARIANT=""
+XKBOPTIONS=""
+
+KBEOF
+   dpkg-reconfigure -f noninteractive keyboard-configuration
+fi
+rm -f /boot/firstrun.sh
+sed -i 's| systemd.run.*||g' /boot/cmdline.txt
 exit 0
