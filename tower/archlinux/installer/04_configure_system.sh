c92d851f0689085300821247f67fb246a44742e6
Ouziel Slama <ouziel@gmail.com>
2023-03-06 19:21:56 +0000

    more iptables rules

--- tower/archlinux/installer/04_configure_system.sh
+++ tower/archlinux/installer/04_configure_system.sh
@@ -49,6 +49,13 @@ iptables -A INPUT -p udp -m conntrack --ctstate NEW -j UDP
 iptables -A INPUT -p tcp --syn -m conntrack --ctstate NEW -j TCP
 iptables -A INPUT -p udp -j REJECT --reject-with icmp-port-unreachable
 iptables -A INPUT -p tcp -j REJECT --reject-with tcp-reset
+iptables -t mangle -I PREROUTING -m rpfilter --invert -j DROP
+iptables -I TCP -p tcp -m recent --update --rsource --seconds 60 --name TCP-PORTSCAN -j REJECT --reject-with tcp-reset
+iptables -D INPUT -p tcp -j REJECT --reject-with tcp-reset
+iptables -A INPUT -p tcp -m recent --set --rsource --name TCP-PORTSCAN -j REJECT --reject-with tcp-reset
+iptables -I UDP -p udp -m recent --update --rsource --seconds 60 --name UDP-PORTSCAN -j REJECT --reject-with icmp-port-unreachable
+iptables -D INPUT -p udp -j REJECT --reject-with icmp-port-unreachable
+iptables -A INPUT -p udp -m recent --set --rsource --name UDP-PORTSCAN -j REJECT --reject-with icmp-port-unreachable
 iptables -A INPUT -j REJECT --reject-with icmp-proto-unreachable
 iptables-save -f /etc/iptables/iptables.rules
 # enable services
