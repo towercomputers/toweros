c3b29701a4aed3cfdd42437c5f6dfae78a0c5c48
Ouziel Slama <ouziel@gmail.com>
2023-03-06 19:04:45 +0000

    setup iptables on thin client os

--- tower/archlinux/installer/04_configure_system.sh
+++ tower/archlinux/installer/04_configure_system.sh
@@ -35,8 +35,25 @@ echo "tower" > /etc/hostname
 # install boot loader
 grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
+# setup firewall
+iptables -N TCP
+iptables -N UDP
+iptables -P FORWARD DROP
+iptables -P OUTPUT ACCEPT
+iptables -P INPUT DROP
+iptables -A INPUT -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+iptables -A INPUT -i lo -j ACCEPT
+iptables -A INPUT -m conntrack --ctstate INVALID -j DROP
+iptables -A INPUT -p icmp --icmp-type 8 -m conntrack --ctstate NEW -j ACCEPT
+iptables -A INPUT -p udp -m conntrack --ctstate NEW -j UDP
+iptables -A INPUT -p tcp --syn -m conntrack --ctstate NEW -j TCP
+iptables -A INPUT -p udp -j REJECT --reject-with icmp-port-unreachable
+iptables -A INPUT -p tcp -j REJECT --reject-with tcp-reset
+iptables -A INPUT -j REJECT --reject-with icmp-proto-unreachable
+iptables-save -f /etc/iptables/iptables.rules
 # enable services
 systemctl enable iwd.service
 systemctl enable dhcpcd.service
 systemctl enable avahi-daemon.service
 systemctl enable docker.service
+systemctl enable iptables.service
