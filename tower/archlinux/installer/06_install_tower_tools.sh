34b9390b8e5262e0c6423018cf03d05a825f39b9
Ouziel Slama <ouziel@stashblox.com>
2023-03-16 12:15:56 +0100

    fix typo

--- tower/archlinux/installer/06_install_tower_tools.sh
+++ tower/archlinux/installer/06_install_tower_tools.sh
@@ -18,7 +18,7 @@ cp *.xz /mnt/home/$USERNAME/.cache/tower/
 arch-chroot /mnt chown -R $USERNAME:$USERNAME /home/$USERNAME/.cache/tower
 
 # put install_dev.sh script in user home
-cp install_dev.sh /home/$USERNAME/
+cp install_dev.sh /mnt/home/$USERNAME/
 arch-chroot /mnt chown -R $USERNAME:$USERNAME /home/$USERNAME/install_dev.sh
 
 # clean cache
