2a4f4c0b1bbd1147a72aef9fd73e4b71bf3c4658
ouziel@gmail.com <Ouziel Slama>
2023-03-28 19:34:58 +0200

    don't install wireless_tools

--- tower/archlinux/installer/03_install_pacman_packages.sh
+++ tower/archlinux/installer/03_install_pacman_packages.sh
@@ -9,7 +9,7 @@ cp pacman.conf /etc/
 pacstrap -K /mnt base linux linux-firmware openssh sudo \
                  iwd grub efibootmgr \
                  dhcpcd git python python-pip avahi \
-                 iw wireless_tools base-devel docker \
+                 iw base-devel docker \
                  archiso lxde xorg-xinit nano vi \
                  nxagent nxproxy nx-headers
 
