b7e4a4d829fc499aa12f2703053c5b760f175abd
ouziel@gmail.com <Ouziel Slama>
2023-04-20 15:40:55 +0200

    launch fluxbox if necessary

--- tower/clis/commands/run.py
+++ tower/clis/commands/run.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import logging
+import time
 
 from tower import sshconf
 from tower import gui
@@ -33,5 +34,12 @@ def execute(args):
     if os.getenv('DISPLAY'):
         gui.run(args.host_name[0], *args.run_command)
     else:
-        logger.error("ERROR: `tower run` requires a running desktop environment. Use `startx` to run the desktop then right click to see all installed application.")
+        # TODO: find a more elegant way or remove ?
+        clean_startup = "sed -i \'/\\[startup\\]/d\' ~/.fluxbox/apps"
+        run_command = " ".join(sys.argv)
+        startup_line = f'[startup] {{{clean_startup} && {run_command}}}'
+        add_startup = f'echo "{startup_line}" >> ~/.fluxbox/apps'
+        cmd = f"{add_startup} && startx"
+        os.system(cmd)
+        #logger.error("ERROR: `tower run` requires a running desktop environment. Use `startx` to run the desktop then right click to see all installed application.")
     
