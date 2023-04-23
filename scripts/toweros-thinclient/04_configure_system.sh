644d3d54f35b528804c7937e3cd42c86a3a940c0
ouziel@gmail.com <Ouziel Slama>
2023-04-23 13:44:27 +0200

    install xterm on the thinclient

--- scripts/toweros-thinclient/04_configure_system.sh
+++ scripts/toweros-thinclient/04_configure_system.sh
@@ -25,8 +25,7 @@ echo "exec startfluxbox" > /home/$USERNAME/.xinitrc
 mkdir -p /home/$USERNAME/.fluxbox
 cp /root/fluxbox_startup /home/$USERNAME/.fluxbox/startup
 cp /usr/share/fluxbox/menu /home/$USERNAME/.fluxbox/menu
-sed -i 's/\[exec\] (xterm) {xterm}/\[include\] (~\/\.fluxbox\/tower-menu)/' /home/$USERNAME/.fluxbox/menu
-sed -i '/\[exec\] (firefox) {}/d' /home/$USERNAME/.fluxbox/menu
+sed -i 's/\[exec\] (firefox) {}/\[include\] (~\/\.fluxbox\/tower-menu)/' /home/$USERNAME/.fluxbox/menu
 # fix ownership
 chown -R $USERNAME:$USERNAME /home/$USERNAME
 # set locales
