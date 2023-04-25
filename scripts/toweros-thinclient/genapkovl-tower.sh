42c71c4a596ff9651c10cce66f6193de77338674
ouziel@gmail.com <Ouziel Slama>
2023-04-25 21:20:41 +0200

    alpine installer progress

---
+++ scripts/toweros-thinclient/genapkovl-tower.sh
@@ -0,0 +1,54 @@
+#!/bin/sh -e
+
+cleanup() {
+	rm -rf "$tmp"
+}
+
+makefile() {
+	OWNER="$1"
+	PERMS="$2"
+	FILENAME="$3"
+	cat > "$FILENAME"
+	chown "$OWNER" "$FILENAME"
+	chmod "$PERMS" "$FILENAME"
+}
+
+rc_add() {
+	mkdir -p "$tmp"/etc/runlevels/"$2"
+	ln -sf /etc/init.d/"$1" "$tmp"/etc/runlevels/"$2"/"$1"
+}
+
+tmp="$(mktemp -d)"
+trap cleanup EXIT
+
+mkdir -p "$tmp"/etc/apk
+makefile root:root 0644 "$tmp"/etc/apk/world <<EOF
+alpine-base
+tower-installer
+EOF
+
+mkdir -p "$tmp"/etc/local.d/
+makefile root:root 0644 "$tmp"/etc/local.d/installer.start <<EOF
+    apk add tower-installer
+	sh /root/installer/alpine_install.sh
+EOF
+
+rc_add local default
+
+rc_add devfs sysinit
+rc_add dmesg sysinit
+rc_add mdev sysinit
+rc_add hwdrivers sysinit
+rc_add modloop sysinit
+
+rc_add hwclock boot
+rc_add modules boot
+rc_add sysctl boot
+rc_add bootmisc boot
+rc_add syslog boot
+
+rc_add mount-ro shutdown
+rc_add killprocs shutdown
+rc_add savecache shutdown
+
+tar -c -C "$tmp" etc | gzip -9n > $HOSTNAME.apkovl.tar.gz
---
+++ scripts/toweros-thinclient/mkimg.tower.sh
@@ -0,0 +1,64 @@
+#sh aports/scripts/mkimage.sh \
+#	--outdir ~/ \
+#	--arch x86_64 \
+#	--repository  http://mirror.kku.ac.th/alpine/v3.17/main \
+#	--repository  http://mirror.kku.ac.th/alpine/v3.17/community \
+#	--repository  file:///home/tower/packages/toweros-thinclient \
+#	--profile tower \
+#	--tag v0.0.1
+
+profile_standard() {
+	title="Standard"
+	desc="Alpine as it was intended.
+		Just enough to get you started.
+		Network connection is required."
+	profile_base
+	profile_abbrev="std"
+	image_ext="iso"
+	arch="aarch64 armv7 x86 x86_64 ppc64le riscv64 s390x"
+	output_format="iso"
+	kernel_addons="xtables-addons"
+	case "$ARCH" in
+	s390x)
+		apks="$apks s390-tools"
+		initfs_features="$initfs_features dasd_mod qeth zfcp"
+		initfs_cmdline="modules=loop,squashfs,dasd_mod,qeth,zfcp quiet"
+		;;
+	ppc64le)
+		initfs_cmdline="modules=loop,squashfs,sd-mod,usb-storage,ibmvscsi quiet"
+		;;
+	riscv64)
+		kernel_flavors="edge"
+		kernel_cmdline="console=tty0 console=ttyS0,115200 console=ttySIF0,115200"
+		kernel_addons=
+		;;
+	esac
+}
+
+profile_tower() {
+	profile_standard
+	profile_abbrev="tower"
+	title="TowerOS-ThinClient"
+	desc="Towercomputer distribution for Thin Client."
+	arch="x86 x86_64"
+	kernel_addons="xtables-addons zfs"
+	boot_addons="amd-ucode intel-ucode"
+	initrd_ucode="/boot/amd-ucode.img /boot/intel-ucode.img"
+	apkovl="genapkovl-tower.sh"
+	apks="$apks
+		tower-installer
+		coreutils openssh sudo nano vim curl 
+		net-tools dhcpcd iptables wpa_supplicant avahi
+		parted rsync python3 py3-pip nx-libs nx-libs-dev
+		alpine-sdk build-base apk-tools alpine-conf busybox fakeroot syslinux xorriso squashfs-tools
+		"
+	local _k _a
+	for _k in $kernel_flavors; do
+		apks="$apks linux-$_k"
+		for _a in $kernel_addons; do
+			apks="$apks $_a-$_k"
+		done
+	done
+	apks="$apks linux-firmware linux-firmware-none"
+}
+
