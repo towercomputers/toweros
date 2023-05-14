73ba5b3304cb56b69ae2c5db054555f1e4402047
Ouziel Slama <ouziel@gmail.com>
2023-05-14 13:57:23 +0200

    fixe iptables rules; install and configure syslog-ng

--- scripts/toweros-thinclient/installer/configure-firewall.sh
+++ scripts/toweros-thinclient/installer/configure-firewall.sh
@@ -1,35 +1,48 @@
 # based on https://wiki.archlinux.org/title/Simple_stateful_firewall
 
+# clean everything
+iptables -P INPUT ACCEPT
+iptables -P FORWARD ACCEPT
+iptables -P OUTPUT ACCEPT
+iptables -t nat -F
+iptables -t mangle -F
+iptables -F
+iptables -X
+
 # log and drop packets
 iptables -N logdrop
-iptables -A logdrop -m limit --limit 5/m --limit-burst 10 --log-prefix "iptables: DROP: " -j LOG
+iptables -A logdrop -m limit --limit 5/m --limit-burst 10 -j LOG --log-prefix "iptables: DROP: "
 iptables -A logdrop -j DROP
+# log and drop packets
+iptables -N logdropm -t mangle
+iptables -A logdropm -t mangle -m limit --limit 5/m --limit-burst 10 -j LOG --log-prefix "iptables: DROP: "
+iptables -A logdropm -t mangle -j DROP
 # log and reject packets with tcp reset
-iptables -N logrejectwithtcpreset
-iptables -A logrejectwithtcpreset -m limit --limit 5/m --limit-burst 10 --log-prefix "iptables: REJECT: " -j LOG
-iptables -A logrejectwithtcpreset -j REJECT --reject-with tcp-reset
+iptables -N logreject-tcpreset
+iptables -A logreject-tcpreset -m limit --limit 5/m --limit-burst 10 -j LOG --log-prefix "iptables: REJECT: "
+iptables -A logreject-tcpreset -p tcp -j REJECT --reject-with tcp-reset
 # log and reject packets with icmp port unreachable
-iptables -N logrejectwithicmpportunreachable
-iptables -A logrejectwithicmpportunreachable -m limit --limit 5/m --limit-burst 10 --log-prefix "iptables: REJECT: " -j LOG
-iptables -A logrejectwithicmpportunreachable -j REJECT --reject-with icmp-port-unreachable
+iptables -N logreject-icmpport
+iptables -A logreject-icmpport -m limit --limit 5/m --limit-burst 10 -j LOG --log-prefix "iptables: REJECT: "
+iptables -A logreject-icmpport -p tcp -j REJECT --reject-with icmp-port-unreachable
 # log and reject packets with icmp protocol unreachable
-iptables -N logrejectwithicmpprotounreachable
-iptables -A logrejectwithicmpprotounreachable -m limit --limit 5/m --limit-burst 10 --log-prefix "iptables: REJECT: " -j LOG
-iptables -A logrejectwithicmpprotounreachable -j REJECT --reject-with icmp-proto-unreachable
+iptables -N logreject-icmpproto
+iptables -A logreject-icmpproto -m limit --limit 5/m --limit-burst 10 -j LOG --log-prefix "iptables: REJECT: "
+iptables -A logreject-icmpproto -p tcp -j REJECT --reject-with icmp-proto-unreachable
 # log and accept
 iptables -N logaccept
-iptables -A logaccept -m limit --limit 5/m --limit-burst 10 --log-prefix "iptables: ACCEPT: " -j LOG
+iptables -A logaccept -m limit --limit 5/m --limit-burst 10 -j LOG --log-prefix "iptables: ACCEPT: "
 iptables -A logaccept -j ACCEPT
 
 # create user defined chains
 iptables -N TCP
 iptables -N UDP
 # reject all forward traffic
-iptables -P FORWARD -j logdrop
+iptables -P FORWARD DROP
 # allow all outbound traffic
 iptables -P OUTPUT ACCEPT
 # drop all input traffic by default
-iptables -P INPUT -j logdrop
+iptables -P INPUT DROP
 # allow ICMP messages
 iptables -A INPUT -m conntrack --ctstate RELATED,ESTABLISHED -j logaccept
 # allow local traffic
@@ -42,21 +55,21 @@ iptables -A INPUT -p icmp --icmp-type 8 -m conntrack --ctstate NEW -j logaccept
 iptables -A INPUT -p udp -m conntrack --ctstate NEW -j UDP
 iptables -A INPUT -p tcp --syn -m conntrack --ctstate NEW -j TCP
 # reject UDP streams with ICMP port unreachable messages
-iptables -A INPUT -p udp -j logrejectwithicmpportunreachable
+iptables -A INPUT -p udp -j logreject-icmpport
 # reject TCP connections with TCP RESET 
-iptables -A INPUT -p tcp -j logrejectwithtcpreset
+iptables -A INPUT -p tcp -j logreject-tcpreset
 # protection against spoofing attacks
-iptables -t mangle -I PREROUTING -m rpfilter --invert -j logdrop
+iptables -t mangle -I PREROUTING -m rpfilter --invert -j logdropm
 # block SYN port scanning
-iptables -I TCP -p tcp -m recent --update --rsource --seconds 60 --name TCP-PORTSCAN -j logrejectwithtcpreset
-iptables -D INPUT -p tcp -j logrejectwithtcpreset
-iptables -A INPUT -p tcp -m recent --set --rsource --name TCP-PORTSCAN -j logrejectwithtcpreset
+iptables -I TCP -p tcp -m recent --update --rsource --seconds 60 --name TCP-PORTSCAN -j logreject-tcpreset
+iptables -D INPUT -p tcp -j logreject-tcpreset
+iptables -A INPUT -p tcp -m recent --set --rsource --name TCP-PORTSCAN -j logreject-tcpreset
 # block UDP port scanning
-iptables -I UDP -p udp -m recent --update --rsource --seconds 60 --name UDP-PORTSCAN -j logrejectwithicmpportunreachable
-iptables -D INPUT -p udp -j logrejectwithicmpportunreachable
-iptables -A INPUT -p udp -m recent --set --rsource --name UDP-PORTSCAN -j logrejectwithicmpportunreachable
+iptables -I UDP -p udp -m recent --update --rsource --seconds 60 --name UDP-PORTSCAN -j logreject-icmpport
+iptables -D INPUT -p udp -j logreject-icmpport
+iptables -A INPUT -p udp -m recent --set --rsource --name UDP-PORTSCAN -j logreject-icmpport
 # reject all remaining incoming traffic with icmp protocol unreachable messages
-iptables -A INPUT -j logrejectwithicmpprotounreachable
+iptables -A INPUT -j logreject-icmpproto
 # open port for avahi
 iptables -I UDP -p udp -m udp --dport 5353 -j logaccept
 
