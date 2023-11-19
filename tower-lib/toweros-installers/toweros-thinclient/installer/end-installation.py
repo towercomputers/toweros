2a04012a77afce031e9f695d05265f1887ee4808
Ouziel Slama <ouziel@gmail.com>
2023-11-19 11:20:57 +0100

    Nice message at the end of installation

---
+++ tower-lib/toweros-installers/toweros-thinclient/installer/end-installation.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+import sys
+import subprocess
+from base64 import b64encode
+
+from rich import print as rprint
+from rich.text import Text
+from rich.console import Console
+    
+
+def main():
+    Console().clear()
+    title = subprocess.run(
+        ['figlet', '-w', '160', 'TowerOS-ThinClient'], 
+        capture_output=True, encoding="UTF-8"
+    ).stdout
+    print(title)
+    print("\n")
+    rprint(Text("Congratulations, toweros is correctly installed!", style="green bold"))
+    print("\n")
+    rprint(Text("Make sure to remove the drive that contains the installation image, then press Enter to reboot.", style="purple bold"))
+    input()
+
+if __name__ == '__main__':
+    sys.exit(main())
--- tower-lib/toweros-installers/toweros-thinclient/installer/install-thinclient.sh
+++ tower-lib/toweros-installers/toweros-thinclient/installer/install-thinclient.sh
@@ -407,7 +407,7 @@ unmount_and_reboot() {
     umount /mnt/boot
     umount /mnt/home
     umount /mnt
-    read -p "Installation done. Make sure to remove the drive that contains the installation image, then press any key to reboot."
+    python $SCRIPT_DIR/end-installation.py
     reboot
 }
 
