47f943832ac6ce506332afed535bfb07033441d6
Ouziel Slama <ouziel@gmail.com>
2023-12-08 17:58:04 +0100

    fix terminal screenlocker

--- tower-lib/toweros-installers/toweros-thinclient/installer/screenlocker.sh
+++ tower-lib/toweros-installers/toweros-thinclient/installer/screenlocker.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
+if [ $(ps -ax | grep 'physlock' | grep -vc 'grep') -gt 0 ]; then
+    echo "physlock already running"
+    exit 0
+fi
+if [ $(ps -ax | grep 'labwc' | grep -vc 'grep') -gt 0 ]; then
+    echo "Labwc already running"
+    exit 0
+fi
+
 LOCK_AFTER=300 # 5 minutes
 LAST_MODIFIED_TTY=$(ls -lt /dev/tty* | head -1 | awk '{print $NF}')
 INACTIVITY_DURATION=$(($(date +%s) - $(date -r $LAST_MODIFIED_TTY +%s)))
 
-if [ $INACTIVITY_DURATION -gt 60 ]; then
-    if ps -a | grep physlock; then
-        echo "physlock already running"
-    else
-        physlock &
-    fi
+if [ $INACTIVITY_DURATION -gt $LOCK_AFTER ]; then
+    physlock &
 fi
