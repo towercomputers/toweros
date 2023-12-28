c9c73db0927d693ab3cd08bbca09f394f5bcc548
Ouziel Slama <ouziel@gmail.com>
2023-12-28 14:45:29 +0100

    fix and tweaks tower-env.sh

--- tower-apks/toweros-thinclient/overlay/etc/profile.d/tower-env.sh
+++ tower-apks/toweros-thinclient/overlay/etc/profile.d/tower-env.sh
@@ -1,5 +1,5 @@
 # prompt format
-export PS1='[\\u@\\H \\W]\\$ '
+export PS1='[\u@\H \W]\$ '
 
 # initialize XDG_RUNTIME_DIR
 if [ -z "$XDG_RUNTIME_DIR" ]; then
@@ -21,15 +21,25 @@ fi
 
 # start actkbd.py and supercronic
 if [ "$(tty)" == "/dev/tty1" ]; then
-    /var/towercomputers/scripts/actkbd.py >/dev/null 2>&1 &
-    supercronic /etc/crontabs/supercronic >/dev/null 2>&1 &
+    # ensure actkbd.py is running
+    if [ $(ps -ax | grep 'actkbd' | grep -vc 'grep') -eq 0 ]; then
+        /var/towercomputers/scripts/actkbd.py >/dev/null 2>&1 &
+    fi
+    # ensure supercronic is running
+    if [ $(ps -ax | grep 'supercronic' | grep -vc 'grep') -eq 0 ]; then
+        supercronic /etc/crontabs/supercronic >/dev/null 2>&1 &
+    fi
+    # ensure that tower.widget is linked
+    if [ ! -L /usr/share/sfwbar/tower.widget ]; then
+        doas ln -s ~/.local/tower/tower.widget /usr/share/sfwbar/tower.widget
+    fi
+    # ensure sudo is an alias to doas
+    if [ ! -L /usr/bin/sudo ]; then
+        doas ln -s /usr/bin/doas /usr/bin/sudo
+    fi
+    # start labwc on login
+    STARTW_ON_LOGIN=${STARTW_ON_LOGIN:-"false"}
+    if [ -z "$DISPLAY" ] && [ "$STARTW_ON_LOGIN" == "true" ]; then
+        dbus-launch labwc;
+    fi
 fi
-
-# start labwc on login
-STARTW_ON_LOGIN=${STARTW_ON_LOGIN:-"false"}
-if [ -z "$DISPLAY" ] && [ "$(tty)" == "/dev/tty1" ] && [ "$STARTW_ON_LOGIN" == "true" ]; then
-    dbus-launch labwc; 
-fi
-
-# ensure that tower.widget is linked
-ln -s ~/.local/tower/tower.widget /usr/local/share/sfwbar/tower.widget || true
