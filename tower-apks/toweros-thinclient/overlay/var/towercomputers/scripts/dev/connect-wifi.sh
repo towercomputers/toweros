831a30bbadc2a50fdca382eccb318c5912d100da
Ouziel Slama <ouziel@gmail.com>
2024-01-04 19:23:54 +0100

    update apk repos for dev

--- tower-apks/toweros-thinclient/overlay/var/towercomputers/scripts/dev/connect-wifi.sh
+++ tower-apks/toweros-thinclient/overlay/var/towercomputers/scripts/dev/connect-wifi.sh
@@ -11,5 +11,9 @@ echo "nameserver 8.8.4.4" | sudo tee -a /etc/resolv.conf
 sudo sed -i 's/#gateway /gateway /g' /etc/network/interfaces
 sudo sed -i 's/#gateway /gateway /g' /etc/local.d/01_init_network.start
 
+# update apk repos
+echo "https://dl-cdn.alpinelinux.org/alpine/latest-stable/main" | sudo tee /etc/apk/repositories
+echo "https://dl-cdn.alpinelinux.org/alpine/latest-stable/community" | sudo tee -a /etc/apk/repositories
+
 # restart network
 sudo rc-service networking restart
